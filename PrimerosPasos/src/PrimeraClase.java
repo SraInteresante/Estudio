6ff61c286b4a211f850dc03214373fd5b4b6df0c
SraIntersante <camila.herrera.alarcon@gmail.com>
2017-08-06 08:14:46 -0400

    primer commit

---
+++ PrimerosPasos/src/PrimeraClase.java
@@ -0,0 +1,11 @@
+public class PrimeraClase {
+	
+
+	public static void main(String[] args) 
+	{
+	    System.out.print ("Hola, mundo");
+		System.out.println();
+	
+	
+		
+}
---
+++ PrimerosPasos/src/Variables.java
@@ -0,0 +1,43 @@
+/**
+ * Clase de variable int y String
+ * @author camila
+ *
+ */
+public class Variables {
+	public static void main(String[] args) {
+		System.out.println("Haré un ejercicio de suma");
+		int variable1;
+		int variable2;
+		variable1 = 1;
+		variable2 = 2;
+		
+		System.out.println("varible1= "+ variable1);
+		System.out.println("varible2= "+ variable2);
+		System.out.println("la suma es "+(variable1+variable2));
+		
+		System.out.println("\n························\n");
+		
+		String variable3;
+		String variable4;
+		variable3 = "s";
+		variable4 = "i";
+		
+		System.out.println("varible3= "+ variable3);
+		System.out.println("varible4= "+ variable4);
+		System.out.println("la suma es "+(variable3+variable4));
+		
+		/*al usar la variable de enteros "int" se sumaron números a la operación,
+		 * así también al usar la variable "String" se sumaron palabras, entonces
+		 * al colocar varible3 = a 4 y variable 4= a 5, se sumaron, una al lado
+		 * de la otra "la suma es 45" lo mismo con la palabras "si"
+		 * 
+		 * String es un objeto, así que siempre va con mayúscula
+		 * int en cambio es un primitivo, al igual que binarios, operaciones
+		 * 
+		 * 
+		 * comandos como Ctrl ____ (espacio) para que java finiquite la operaciòn
+		 * ej. syso Ctrl (espacio) y termina con System.out.println ()
+		 * Ctrl f = justifica las oraciones
+		 */
+	}
+}
